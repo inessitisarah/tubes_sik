2d21b92613e506de8ae8e1dc3190d2056a9341e7
inessitisarah <78847095+inessitisarah@users.noreply.github.com>
2022-11-30 17:21:34 +0700

    ngubah UI login dikit

--- login.php
+++ login.php
@@ -1,16 +1,37 @@
+<!-- Connect to the DB -->
+<?php include "include/configDB.php"; ?>
+
 <!DOCTYPE html>
 
 <html>
 
+
+    <!-- HEAD -->
 <head>
+    <title>Homepage : Puskesmas Ganesha</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link href="layout/styles/layout.css" rel="stylesheet" type="text/css" media="all">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <!-- STYLE -->
+    <?php include "templates/style.php"; ?>
 
     <title>LOGIN</title>
 
-    <link rel="stylesheet" type="text/css" href="style.css">
 
 </head>
 
 <body>
+    <h3>LOGIN</h3>
+
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <?php include "templates/navbarWithoutMenu.php"; ?>
+    <?php include "templates/style.php"; ?>
+
 
      <form action="login.php" method="post">
 
@@ -35,5 +56,6 @@
      </form>
 
 </body>
+<?php include "templates/footer.php"; ?>
 
 </html>
