770715401a19d5400c57727c22ed009eb99f7e8f
Dennisa Galuh Sekar Kinanthi <18319017@std.stei.itb.ac.id>
2022-12-05 21:09:38 +0700

    Merge branch 'main' of https://github.com/inessitisarah/tubes_sik

--- templates/navbarWithMenuPasien.php
+++ templates/navbarWithMenuPasien.php
@@ -1,6 +1,6 @@
 <div class="w3-top">
   <div class="w3-bar w3-white w3-wide w3-padding w3-card">
-    <a href="pagePasien.php" class="w3-bar-item w3-button"><b>PUSKESMAS</b> Ganesha</a>
+    <a href="index.php" class="w3-bar-item w3-button"><b>PUSKESMAS</b> Ganesha</a>
     
     <div class="w3-right">
       <a href="./logout.php" class="w3-bar-item w3-button">Logout</a>
