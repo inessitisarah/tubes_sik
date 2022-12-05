20d0be5096e78d8f532ddb211d1c9405cd1d8f69
18319027 <18319027@std.stei.itb.ac.id>
2022-12-05 20:40:26 +0700

    Update sidebarApoteker.php

--- templates/sidebarApoteker.php
+++ templates/sidebarApoteker.php
@@ -2,6 +2,6 @@
 <nav class="w3-sidebar w3-bar-block w3-card w3-top w3-xlarge w3-animate-left" style="display:none;z-index:2;width:40%;min-width:300px" id="mySidebar">
   <a href="javascript:void(0)" onclick="w3_close()"
   class="w3-bar-item w3-button">Close Menu</a>
-  <a href="./stokObat.php#stok" onclick="w3_close()" class="w3-bar-item w3-button">Stok Obat</a>
-  <a href="./preskripsiObat.php#preskripsi" onclick="w3_close()" class="w3-bar-item w3-button">Preskripsi Obat</a>
+  <a href="./stokObat.php" onclick="w3_close()" class="w3-bar-item w3-button">Stok Obat</a>
+  <a href="./preskripsiObat.php" onclick="w3_close()" class="w3-bar-item w3-button">Preskripsi Obat</a>
 </nav>
